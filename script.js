ede10a194975002041dd7f41c6b4f048c39415db
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-11 15:37:16 -0500

    Arithmetic average working properly

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ the list elements (no limit elements number). The program has to show the unorde
 1. Insert the formula for the average
 */
 
+var numList = [150, 45, 180, 7];
+var listSum;
+var average;
+
+//1.
+listSum = numList.reduce(sum);
+function sum(totalSum, listElement){
+    totalSum = totalSum + listElement;
+    return totalSum;
+}
+
+function listAverage(listSum){
+    let listAverage = listSum / numList.length;
+    return listAverage;
+}
+
