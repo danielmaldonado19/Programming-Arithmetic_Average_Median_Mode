e06b449ea3b4c644716d14c8611d8b326fc7f128
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-11 15:15:43 -0500

    Initial files boilerplate and program description

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+Program: It has to calculate the arithmetic average, median and mode for any given list. The user should insert
+the list elements (no limit elements number). The program has to show the unordered list (as the user inserted it), the ordered list and the result for each operation.
+1. Insert the formula for the average
+*/
+
