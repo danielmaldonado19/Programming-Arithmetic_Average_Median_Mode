42f12ec5a32d5ac8bbb2d2f7e47439c89ec22fde
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-12 17:40:41 -0500

    Little change

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /*
-Program: It has to calculate the arithmetic average, median and mode for any given list. The user should insert
-the list elements (no limit elements number). The program has to show the unordered list (as the user inserted it), the ordered list and the result for each operation.
+Program: It has to calculate the arithmetic average, median and mode for any given list. The user should insert the list elements (there is not a limit of elements). The program has to show the unordered list (as the user inserted it), the ordered list and the result for each operation.
 1. Insert the formula for the average
 2. Insert the formula for the median
 */
@@ -44,6 +43,8 @@ function listMedian(array){
 
     return median;
 }
+
+
 
 
 
